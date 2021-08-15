b2000410cec37266c330ba805ab6bea3b567f449
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-08-14 23:54:23 -0600

    got server up

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require('path');
+
+var PORT = process.env.PORT || 8081;
+
+var app = express();
+
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+//app.use(express.static("public"));
+app.use(express.static(__dirname + 'public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+// Import routes and give the server access to them.
+/* var routes = require("./routes/index");
+
+app.use(routes); */
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
