850d8a7fa0ef9083aeee01b1fe23396bf992335b
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-08-15 09:29:16 -0600

    route/

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + 'public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("index", function(req, res) {
+app.get("/", function(req, res) {
     // res.send("Welcome to the Star Wars Page!")
     res.sendFile(path.join(__dirname, "index.html"));
   });
